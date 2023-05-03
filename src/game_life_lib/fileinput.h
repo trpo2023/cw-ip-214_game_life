d617966c267d8c61da2aa0531854ff4d6dafde49
MacIT54 <daniil.ledkin@mail.ru>
2023-05-03 16:53:23 +0700

    Add fileinput function

---
+++ src/game_life_lib/fileinput.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <../src/game_life_lib/display.h>
+#include <cstring>
+#include <fstream>
+#include <string>
+void file_input(bool grid[gridsize + 1][gridsize2 + 1], int counter_);

5ce482121a4bd7625c440f7e60eaf2d4aac32896
MacIT54 <daniil.ledkin@mail.ru>
2023-05-16 20:22:03 +0700

    Update main menu

--- src/game_life_main/main.cpp
+++ src/game_life_main/main.cpp
@@ -12,47 +12,52 @@
 int main()
 {
     std::cout << "\n-------------------Welcome to the game - "
-                 "life----------------------\n"
-              << "-----------------Please, select start game "
-                 "option------------------\n"
-              << "----------Input 'r', if random, input 'f', if from "
-                 "file------------\n"
-              << "---------------Then the program will run for 30 "
-                 "seconds------------\n"
-              << "---You can end the program by entering '0' or by pressing "
-                 "CTRL+Z---\n";
+                 "life----------------------\n";
     const int gridsize = 30;
     const int gridsize2 = 80;
     bool grid[gridsize + 1][gridsize2 + 1] = {};
-    char game_option[3], strings[gridsize + 1][gridsize2 + 1], str[gridsize];
+    char game_option[3], strings[gridsize + 1][gridsize2 + 1];
     int rn, k = 0, k2 = 0, n = 0, counter_ = 0;
-
-    std::cin >> game_option;
-    if (strcmp(game_option, "r") == 0) {
-        random_input(rn, grid);
-    } else if (strcmp(game_option, "f") == 0) {
-        file_input(grid, counter_);
-    } else {
-        std::cout << "Your input is not defined\n";
-    }
-    time_t start_time = time(NULL);
-    bool continue1 = 0;
-    while (true) {
-        time_t current_time = time(NULL);
-        if (current_time - start_time >= 30) {
-            start_time = current_time;
-            std::cout << "Continue? Input 1, or 0 to exit: ";
-            std::cin >> continue1;
-            if (!continue1) {
-                exit(0);
+    do {
+        for (int i = 0; i < gridsize + 1; i++) {
+            for (int j = 0; j < gridsize2 + 1; j++) {
+                grid[i][j] = 0;
             }
         }
-        display(grid);
-        live_or_die(grid);
-        unsigned int nanoseconds = 150000;
-        usleep(1 * nanoseconds);
-        system("clear");
-    }
+        print_menu();
+        std::cin >> game_option;
+        if (strcmp(game_option, "r") == 0) {
+            random_input(rn, grid);
+        } else if (strcmp(game_option, "f") == 0) {
+            file_input(grid, counter_);
+        } else if (strcmp(game_option, "0") == 0) {
+            exit(0);
+        } else {
+            std::cout << "Your input is not defined\n\n";
+            continue;
+        }
+        time_t start_time = time(NULL);
+        char continue1[3];
+        do {
+            time_t current_time = time(NULL);
+            if (current_time - start_time >= 3) {
+                start_time = current_time;
+                std::cout << "Continue? Input any value, m to menu, or 0 to "
+                             "exit: ";
+                std::cin >> continue1;
+                if (strcmp(continue1, "0") == 0) {
+                    exit(0);
+                } else if (strcmp(continue1, "m") == 0) {
+                    break;
+                }
+            }
+            display(grid);
+            live_or_die(grid);
+            unsigned int nanoseconds = 150000;
+            usleep(1 * nanoseconds);
+            system("clear");
+        } while (1);
+    } while (1);
 
     return 0;
 }
